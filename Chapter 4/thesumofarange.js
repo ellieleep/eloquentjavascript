f1bbef0907f683168b57fe51f222c57c2feee356
ellieleep <32715340+ellieleep@users.noreply.github.com>
2021-11-06 20:24:51 -0400

    chapter 4

---
+++ Chapter 4/thesumofarange.js
@@ -0,0 +1,25 @@
+function range(start, end, step= 1){
+    let rangeArray = [];
+    if(step > 0) {
+        for (let i = start; i <= end; i += step) {
+            rangeArray.push(i);
+        }
+    }
+    else{
+        for (let y = start; y >= end; y -= -step) {
+            rangeArray.push(y);
+        }
+    }
+    return rangeArray;
+}
+
+function sum(arrayToSum){
+    let sum = 0;
+    for(let z = 0; z < arrayToSum.length; z++){
+        sum += arrayToSum[z]
+    }
+    return sum
+}
+
+console.log(range(5,2, -1))
+console.log(sum(range(5,2, -1)))
